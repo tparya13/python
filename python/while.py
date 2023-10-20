aab00b902dcfcc5fb9dcc3b7ff5fddb35897352a
arya <aryatheruparambil@gmail.com>
2023-10-20 09:00:50 +0530

    file added

--- python/while.py
+++ python/while.py
@@ -326,5 +326,93 @@
 #     print("false")
 
 
+"""discount"""
+# a=int(input("enter the amound:" ))
+# d=int(input("enter the discount"))
+# def discount(b,c):
+#     return (b/c*100)
+
+
+"""local and global"""
+
+# a=10
+# def asd():
+#     print("inside asd()",a)
+# asd()
+# print("outside asd()",a)    
+
+
+
+"""local"""
+# def abc():
+#     a=20
+#     print(a)
+# abc()  
+
+"""global"""
+# a=30
+# def abc():  
+#     print(a)
+# abc()  
+
+"""enclosed"""  
+# def abc():
+#     a=10
+#     def adc():    
+#         print(a)
+#     adc()
+# abc()    
+
+"""recursion"""
+# def asd():
+#     print("a")
+#     asd()
+# asd()    
+
+
+
+# a=1
+# def asd(b):
+#     b=b+1
+#     print(b)
+#     asd(b)
+# asd(a)    
+
+
+# a=1
+# def asd(b):
+#     if b<=10:
+       
+#         print(b)
+#         b=b+1
+#         asd(b)
+# asd(a)        
+          
+          
+          
+""" recursion function"""
+
+# def fact(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n*fact(n-1)
+# res=fact(0) 
+# print(res) 
+
+
+# a=100
+# def asd(b):
+#     if b>=1:
+#         print(b)
+#         b=b-1
+#         asd(b)
+# asd(a)                     
+                                                                                                                                                     
+    
+
+    
+ 
+
           
 
