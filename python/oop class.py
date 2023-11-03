2e34d01d23293d99a01a6b67284784ec125f17e3
arya <aryatheruparambil@gmail.com>
2023-11-03 09:07:39 +0530

    file added

--- python/oop class.py
+++ python/oop class.py
@@ -105,7 +105,7 @@
 # ob=Granpa()
 # ob.kashandi()   
 
-# """multiple inheritence"""
+"""multiple inheritence"""
 # class Father:
 #     def driver(self):
 #         print("driver")
@@ -118,7 +118,93 @@
 # g=girl()
 # g.enginner()   
 # g.cook()   
-# g.driver()          
+# g.driver()  
+
+
+"""prog"""
+class libraryitem:
+    def title(self):
+        print("voice of nature")
+    def author(self):
+        print("mark john")
+    def id(self):
+        print(201202)
+class book(libraryitem):
+    def no__of__pages(self):
+        print("420")
+    def read_book(self):
+        print("reading the book")
+class dvd(libraryitem):
+    def duration(self):
+        print("4 hours")
+    def dvd_play(self):
+        print("playing the dvd")
+b=book()
+b.author()
+b.id()
+b.no__of__pages()
+b.read_book()
+b.title()                  
+        
+c=dvd()
+c.duration()
+c.dvd_play()        
+
+
+
+"""encapsulation"""
+# class bank:
+#     def __init__(self,accno,__savings):  
+#         self.accno=accno
+#         self.savings=__savings
+#     def deposite(self,dep):
+#         self.savings+=dep
+#     def display(self):
+#         print("savings=",self.savings)
+# anu=bank(2345612346,3000)
+# anu.deposite(4000)
+# anu.display()        
+           
+"""private"""          
+# class A:
+#     __a=10
+#     def disp(self):
+#         print(self.__a)
+# obj=A()
+# obj.disp()   
+
+"""protected"""
+# class A:
+#     _a=10
+#     def disp(self):
+#         print(self._a)
+# obj=A()
+# obj.disp() 
+# print(obj._a)   
+
+"""abstract"""
+# from abc import ABC,abstractclassmethod
+# class vehicle(ABC):
+#     def engine(self):
+#         print("engine provide")
+#     @abstractclassmethod    
+#     def gear(self):
+#         pass
+# class car(vehicle):
+#     def gear(self):
+#         print("automatic gear")
+# class truck(vehicle):      
+#     def wheel(self):
+#         print("heavy")
+#     def gear(self):
+#         print("manuel gear")  
+# c=car()
+# c.gear()
+# t=truck()  
+# t.gear()                    
+                             
+                       
+
                                      
                                    
     
