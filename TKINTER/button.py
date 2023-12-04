7e1e6ef417aef6993af9f19c968ff97bab5a088c
tparya13 <143874371+tparya13@users.noreply.github.com>
2023-12-04 09:34:09 +0530

    Add files via upload

---
+++ TKINTER/button.py
@@ -0,0 +1,5 @@
+from tkinter import*
+root=Tk()
+button=Button(root,text="Login",bg="red",fg="white")
+button.pack()
+root.mainloop()
---
+++ TKINTER/fram.py
@@ -0,0 +1,7 @@
+from tkinter import*
+root=Tk()
+frame=Frame(root)
+frame.pack()
+button=Button(frame,text="Login",bg="red",fg="white")
+button.pack()
+root.mainloop()
